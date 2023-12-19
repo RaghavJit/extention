d01e9438d0966232d3bb052d6a8868da8a8d4d01
RaghavJit <raghavjit002@gmail.com>
2023-12-20 01:57:12 +0530

    hi

--- brain.js
+++ brain.js
@@ -5,34 +5,39 @@ btn.addEventListener('click', function() {
     mainFunction()          
 });
 
-function mainFunction() {
-    // Create a new XMLHttpRequest object
-    var xhttp = new XMLHttpRequest();
+async function mainFunction() {
+    try {
+        // Use the fetch API to make the GET request
+        const response = await fetch("http://localhost:5000/ping");
 
-    // Define the callback function to handle the response
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
+        // Check if the request was successful (status code 200)
+        if (response.ok) {
+            // Convert the response to JSON
+            const data = await response.json();
+
             // Update the content of the product_list element with the response
-            list.innerHTML = this.responseText;
-            console.log(this.responseText)
-            this.response.forEach(product => {
-                  const productDiv = document.createElement("div");
-                  productDiv.innerHTML = `
+            list.innerHTML = JSON.stringify(data); // Update this line based on your actual response structure
+
+            // Iterate through each product and create HTML elements
+            data.forEach(product => {
+                const productDiv = document.createElement("div");
+                productDiv.innerHTML = `
                     <h3>${product.title}</h3>
                     <p>Brand: ${product.brand}</p>
                     <p>Price: ${product.price}</p>
                     <a href="${product.product_url}" target="_blank">Product Link</a>
                     <img src="${product.image_url}" alt="${product.title}" style="max-width: 200px; max-height: 200px;">
                     <hr>
-                  `;
-                  productListContainer.appendChild(productDiv);
-                });
+                `;
+                productListContainer.appendChild(productDiv);
+            });
+        } else {
+            console.error(`Error: ${response.status} - ${response.statusText}`);
         }
-    };
-
-    // Open a GET request to the specified URL
-    xhttp.open("GET", "http://localhost:5000/ping", true);
-
-    // Send the request
-    xhttp.send();
+    } catch (error) {
+        console.error("Error:", error);
+    }
 }
+
+// Call the main function
+mainFunction();
